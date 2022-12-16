76960c5499c4b8c1f0129cf278485a59094d65ac
JAston <jacob.aston1@gmail.com>
2022-12-15 18:26:59 -0700

    Added file structure and changed server.js to index.js.

---
+++ index.js
@@ -0,0 +1,50 @@
+// Import and require mysql2
+const mysql = require('mysql2');
+const inquirer = require("inquirer");
+
+// const PORT = process.env.PORT || 3001;
+
+// // Express middleware
+
+// // Connect to database
+// const db = mysql.createConnection(
+//   {
+//     host: 'localhost',
+//     // MySQL username,
+//     user: 'root',
+//     // MySQL password
+//     password: 'password',
+//     database: 'courses_db'
+//   },
+//   console.log(`Connected to the courses_db database.`)
+// );
+
+inquirer
+  .createPromptModule([
+    {
+        type: "list",
+        message: "Pick a function",
+        name: "function",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Change an employee role"],
+    }
+  ])
+
+// Hardcoded query: DELETE FROM course_names WHERE id = 3;
+
+// db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// // Query database
+// db.query('SELECT * FROM course_names', function (err, results) {
+//   console.log(results);
+// });
+
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
+
