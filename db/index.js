24a565001e007c72b29606aa299c4b9ed4d8abbf
JAston <jacob.aston1@gmail.com>
2022-12-18 21:31:45 -0700

    fixed template literal error

--- db/index.js
+++ db/index.js
@@ -24,7 +24,7 @@ const viewAllEmployees = () => {
   connection.query(
     `SELECT e.employee_id, e.first_name, e.last_name, r.title, d.department, r.salary, e.manager_id 
     FROM employees e LEFT JOIN roles r ON e.role_id = r.role_id 
-    LEFT JOIN departments d ON r.department_id = d.department_id, function (err, results) {
+    LEFT JOIN departments d ON r.department_id = d.department_id`, function (err, results) {
     if (err) {
       console.log(err);
     }
